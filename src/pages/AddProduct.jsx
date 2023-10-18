d021522610f6ee887ce85a4d6fbc6a332bd055c7
Safayat-Hasan <safayathasananim20@gmail.com>
2023-10-19 01:32:43 +0600

    added addProduct page and implemented server and db

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,9 +1,110 @@
+import Swal from "sweetalert2";
 
 
 const AddProduct = () => {
+
+    const handleAddProduct = event => {
+        event.preventDefault();
+        const form = event.target;
+        const image = form.image.value;
+        const name = form.name.value;
+        const brand = form.brand.value;
+        const type = form.type.value;
+        const price = form.price.value;
+        const short = form.short.value;
+        const rating = form.rating.value;
+
+        const newProduct = { image, name, brand, type, price, short, rating };
+        console.log(newProduct);
+
+        fetch("http://localhost:5000/products", {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newProduct)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Product added',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    })
+                }
+            })
+    }
+
     return (
-        <div>
-            
+        <div className="bg-emerald-100 mb-20 md:p-24 w-full">
+            <h2 className="text-3xl font-extrabold text-center mb-6">Add a Product</h2>
+            <form onSubmit={handleAddProduct}>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-8 gap-4">
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text font-bold">Image</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="image" placeholder="Image URL" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text font-bold">Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="name" placeholder="Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text font-bold">Brand Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="brand" placeholder="Brand Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text font-bold">Type</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="type" placeholder="Type" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text font-bold">Price</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="price" placeholder="Price" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text font-bold">Short Description</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="short" placeholder="Short Description" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text font-bold">Rating</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="rating" placeholder="Rating" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                <input type="submit" value="Add Product" className="btn btn-block bg-lime-300" />
+            </form>
         </div>
     );
 };
